dc3692fa51e43d12564b2846111c39e51f9bb03f
Airidas <airidas.venskus@gmail.com>
2020-08-09 23:33:04 +0200

    App init

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="content">content</div>
+    </Router>
+  );
+}
+
+export default App;
