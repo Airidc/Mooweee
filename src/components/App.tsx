93bf36e8f159e1a1174839ee1d3b08b8d589a638
Airidas <airidas.venskus@gmail.com>
2020-08-10 22:04:01 +0200

    Small header adjustments for responsiveness

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 import { store } from "../store/store";
 
 import Header from "./shared/Header";
+import Home from "./Home/Home";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Header></Header>
-        <div className="content"></div>
+        <div className="content">
+          <Route exact path="/" component={Home}></Route>
+        </div>
       </Router>
     </Provider>
   );
