abb61dc495f3027967767b1a1f1c0fe84c8d43a8
Airidas <airidas.venskus@gmail.com>
2020-08-14 17:01:17 +0200

    deploy

---
+++ precache-manifest.3ae3130106ac8e2de5e65df01346ba00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf2bd7288f25a8434a75f5fbd1b294e",
+    "url": "/Mooweee/index.html"
+  },
+  {
+    "revision": "57cc46b868f9308edd34",
+    "url": "/Mooweee/static/css/main.af2bd669.chunk.css"
+  },
+  {
+    "revision": "962dcd7d221c3c9c6b05",
+    "url": "/Mooweee/static/js/2.a885091b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mooweee/static/js/2.a885091b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57cc46b868f9308edd34",
+    "url": "/Mooweee/static/js/main.639b0751.chunk.js"
+  },
+  {
+    "revision": "f007814c3be7dc37d590",
+    "url": "/Mooweee/static/js/runtime-main.ab794a50.js"
+  },
+  {
+    "revision": "5b0c2cbfabadaf8602817657059b4b7c",
+    "url": "/Mooweee/static/media/logo.5b0c2cbf.svg"
+  }
+]);
